15c03125a3955d9dd0205168b4f54a55a763bddf
Seth Schwartz <sschwartz214@gmail.com>
2018-07-05 15:46:18 -0600

    adds edit functionality

---
+++ spec/features/user_edits_an_article_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'user edits an article' do
+  describe 'they link from a show page' do
+    describe 'they fill in an edit field' do
+      it 'and submit displays the updated information on a show page' do
+        article = Article.create!(title: 'Title', body: 'Body')
+
+        visit article_path(article)
+        click_on 'Edit'
+
+        expect(current_path).to eq(edit_article_path(article))
+
+        fill_in('article[title]', with: "New Title")
+        fill_in('article[body]', with: "New Body")
+        click_on "Update"
+
+        expect(page).to have_content("New Title")
+        expect(page).to have_content("New Body")
+      end
+    end
+  end
+end
