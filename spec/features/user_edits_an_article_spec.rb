889edb5bb927e71df2f9c0554f9619acf319ca67
Seth Schwartz <sschwartz214@gmail.com>
2018-07-05 21:57:52 -0600

    adds flash message

--- spec/features/user_edits_an_article_spec.rb
+++ spec/features/user_edits_an_article_spec.rb
@@ -17,6 +17,7 @@ describe 'user edits an article' do
 
         expect(page).to have_content("New Title")
         expect(page).to have_content("New Body")
+        expect(page).to have_content("Article 'New Title' Updated!")
       end
     end
   end
